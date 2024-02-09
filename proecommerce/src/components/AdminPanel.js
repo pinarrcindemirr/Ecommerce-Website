9a64956b3d6040a78fc30bf8fb33baea14ec7116
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-02-09 09:25:51 +0300

    commit

--- proecommerce/src/components/AdminPanel.js
+++ proecommerce/src/components/AdminPanel.js
@@ -15,17 +15,3 @@ const AdminPanel = () => {
 };
 
 export default AdminPanel;
-
- {/*
-  <div>
-  <h3>Product List</h3>
-  <ul>
-   {products.map((product, index) => (
-    <li key={index}>
-     <img src={product.image} alt={product.name} style={{ width: '100px', height: '100px' }} />
-      <p>{product.name} - ${product.price}</p>
-     </li>
-    ))}
-   </ul>
-  </div>
-   */}
